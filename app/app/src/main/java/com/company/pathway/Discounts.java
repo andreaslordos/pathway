a6867e596656166bf57604f93b49d3ea92630c61
cfalas <34488895+cfalas@users.noreply.github.com>
2019-06-20 11:46:08 +0300

    Added dialog to claim discounts

--- app/app/src/main/java/com/company/pathway/Discounts.java
+++ app/app/src/main/java/com/company/pathway/Discounts.java
@@ -1,7 +1,12 @@
 package com.company.pathway;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class Discounts extends AppCompatActivity {
 
@@ -9,5 +14,45 @@ public class Discounts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discounts);
+
+        View.OnClickListener notenough = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Discounts.this, "You don't have enough points", Toast.LENGTH_SHORT).show();
+            }
+        };
+        final int points = getSharedPreferences("POINTS", MODE_PRIVATE).getInt("points", 542);
+        LinearLayout first = findViewById(R.id.first);
+        LinearLayout second = findViewById(R.id.second);
+        LinearLayout third = findViewById(R.id.third);
+        LinearLayout fourth = findViewById(R.id.fourth);
+        fourth.setOnClickListener(notenough);
+        third.setOnClickListener(notenough);
+        second.setOnClickListener(notenough);
+        if (points > 600) {
+            first.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(Discounts.this)
+                            .setTitle("Claim reward")
+                            .setMessage("Are you sure you want to claim this reward? 600 points will be removed from your account, and you will be notified about your rewad from your insurance company.")
+
+                            // Specifying a listener allows you to take an action before dismissing the dialog.
+                            // The dialog is automatically dismissed when a dialog button is clicked.
+                            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    getSharedPreferences("POINTS", MODE_PRIVATE).edit().putInt("points", points - 600).apply();
+                                }
+                            })
+
+                            // A null listener allows the button to dismiss the dialog and take no further action.
+                            .setNegativeButton(android.R.string.cancel, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            });
+        } else {
+            first.setOnClickListener(notenough);
+        }
     }
 }
--- app/app/src/main/java/com/company/pathway/PointsActivity.java
+++ app/app/src/main/java/com/company/pathway/PointsActivity.java
@@ -1,17 +1,25 @@
 package com.company.pathway;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
+
+import me.tankery.lib.circularseekbar.CircularSeekBar;
 
 public class PointsActivity extends AppCompatActivity {
-
+    SharedPreferences.Editor sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points);
+        sharedPreferences = getSharedPreferences("POINTS", MODE_PRIVATE).edit();
+        sharedPreferences.putInt("points", 642).apply();
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -20,5 +28,24 @@ public class PointsActivity extends AppCompatActivity {
                 startActivity(new Intent(PointsActivity.this, Discounts.class));
             }
         });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        SharedPreferences prefs = getSharedPreferences("POINTS", MODE_PRIVATE);
+        int points = prefs.getInt("points", 542);
+        TextView point = findViewById(R.id.points);
+        point.setText(String.valueOf(points));
+        CircularSeekBar seekBar = findViewById(R.id.circle);
+        TextView pointsleft = findViewById(R.id.left);
+        if(points<600){
+            seekBar.setProgress(100*(((float)points)/600));
+            pointsleft.setText(MessageFormat.format("{0} {1}", 600 - points, getString(R.string.points_left)));
+        }
+        else if(points<1000){
+            seekBar.setProgress(((float)points)/10);
+            pointsleft.setText(MessageFormat.format("{0} {1}", 1000 - points, getString(R.string.points_left)));
+        }
     }
 }
